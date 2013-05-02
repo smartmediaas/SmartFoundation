ee06cd7eb5796ad25364e40a5257cb3b375569b7
Olabogen <ola@smartmedia.no>
2013-05-02 14:21:57 +0200

    fixed sidebar width

--- sidebar.php
+++ sidebar.php
@@ -1,5 +1,5 @@
 <?php if(is_active_sidebar('sidebar-1')): ?>
-	<section id="secondary" class="large-8 columns widget-area" role="complementary">		
+	<section id="secondary" class="large-4 columns widget-area" role="complementary">		
 		<?php dynamic_sidebar('sidebar-1'); ?>
 	</section>
 <?php endif; ?>
